a50042aa2df4421dfde02a001f8e237ce6eb6210
dickdral <dick.dral@detora.nl>
2017-01-08 10:48:01 +0100

    Initial commit

--- server/js/apexreport2columns.js
+++ server/js/apexreport2columns.js
@@ -1,102 +1,79 @@
-// APEX Report to Columns
-// Author: Dick Dral
+// APEX Resize Dialog
+// Author: Dick Dral (Detora)
 // Version: 1.0
 
 // global namespace
-var apexreport2columns = {
-    // parse string to boolean
-    parseBoolean: function(pString) {
-        var pBoolean;
-        if (pString.toLowerCase() == 'true') {
-            pBoolean = true;
-        }
-        if (pString.toLowerCase() == 'false') {
-            pBoolean = false;
-        }
-        if (!(pString.toLowerCase() == 'true') && !(pString.toLowerCase() == 'false')) {
-            pBoolean = undefined;
-        }
-        return pBoolean;
-    },
+var apexresizedialog = {
 
-    // helper function to get right text
-    getText: function(pSource, pText, pItem, pElement) {
-        var vText;
-        if (pSource == 'TEXT') {
-            vText = pText;
-        } else if (pSource == 'ITEM') {
-            vText = $v(pItem);
-        } else if (pSource == 'TITLE') {
-            vText = $(pElement).attr('title');
-        }
-        return vText;
-    },
-    
-    // function doing the real work
-    distribute: function( tabSelector, numCols, direction )  {
-      var numRows  = 0;  
-	  var curtable = null;
-	
-	  // check parameter direction and apply default value
-	  direction    = ( direction ) ? direction : 'vertical';
-	
-	  // calculate the number of rows per column
-	  var table      = $(tabSelector).addClass('reportColumn'); 
-	  var rowsPerCol = Math.ceil( ( $(table).find(' tr').length-1 )/numCols);
-	  var randomNum  = Math.floor((Math.random() * 10000000) + 1);
-	  var baseName   = 'reportColumn_'+randomNum;   
+resize_dialog: function ( center_dialog, dialog_margin )
+{
+  console.log('perform dialog resize');
   
-	  // create table template for columns 
-	  var template = $(table).clone();
-	  $(template).find('tr').has('td').remove();
-  
-
-	  if ( direction.toLowerCase() != 'horizontal') 
-	  {  
-		 // VERTICAL 
-		 for ( i = 2; i <= numCols; i++)  
-		 {  curtable = $(template).clone().attr('id',baseName+i).appendTo( $(table).parent() );
-			rows = $(table).find('tr').has('td').slice( rowsPerCol, 2*rowsPerCol).detach();
-			$(curtable).find('tbody').append(rows);
-		 }  
-	  }
-	  else
-	  {
-		 // HORIZONTAL
-		 // create columns
-		 for ( i = 2; i <= numCols; i++)    
-		 {  curtable = $(template).clone().attr('id',baseName+i).appendTo( $(table).parent() );  
-		 }  
- 
-		 // distribute the rows over the columns
-		 var id = 1;  
-		 $(tabSelector).find('tbody tr').each( function(index)  
-		 { 
-			// move cell if not in first column  
-			if ( id > 1  ) { $('#'+baseName+id+' tbody').append( $(this) );  }
-
-			// restart with column 1 when behind last column
-			id = ( id < numCols ) ? id + 1 : 1;
-		 } );   
-	  }    
-
-    },
+  var header  = $('.t-Dialog-header');
+  var height  = parseInt($(header).height());  
     
+  var body    = $('.t-Dialog-body');
+  var padding = parseInt($(body).css('padding-top')) + parseInt($(body).css('padding-bottom'));
+  height += padding;
+    
+  var container = $(body).find('.container').first();
+  height   += parseInt($(container).height());  
+    
+  var footer    = $('.t-Dialog-footer');
+  height   += parseInt($(footer).height());  
+
+  var dialog_window    = window.parent.$('.ui-dialog');
+  var dialog_titlebar  = window.parent.$('.ui-dialog-titlebar');
+  var dialog_container = window.parent.$('.ui-dialog-content');
+
+  var parent_height    = window.parent.innerHeight;
+  var titlebar_height  = parseInt(dialog_titlebar.css('height'));
+  var dialog_top       = parseInt(dialog_window.css('top'));
+
+  center_dialog = ( center_dialog == true || center_dialog == false ) ? center_dialog : false;
+  dialog_margin = ( dialog_margin == undefined || dialog_margin == null ) ? 20 : parseInt(dialog_margin);
+  console.log(center_dialog,dialog_margin,height);
+  
+  if ( center_dialog )
+      {
+          // how much height is available?
+          var available_height = parent_height - titlebar_height - 2* dialog_margin;
+
+          // adjust height if necessary
+          height  = ( height < available_height ) ? height : available_height;
+
+          // determine and set position
+          var top = ( parent_height - height - titlebar_height ) / 2;
+          dialog_window.css('top',top);
+
+      }
+  else
+      {          
+          // how much height is available?
+          var available_height = parent_height - dialog_top - titlebar_height - dialog_margin;
+console.log('parent_height - dialog_top - titlebar_height=',parent_height,dialog_top,titlebar_height);
+          // adjust height if necessary
+          height = ( height <= available_height ) ? height : available_height;
+      }
+
+	  console.log(height,dialog_container);
+    
+  $(dialog_container).height(height);
+},
+
     // function that gets called from plugin
     doIt: function() {
         // plugin attributes
         var daThis = this;
-        var vElementsArray = daThis.affectedElements;
-        var vTabSelector   = '#'+$(vElementsArray[0]).attr('id')+' table.t-Report-report';
-        var vNumcols = daThis.action.attribute01;
-        var vDirection = daThis.action.attribute02;
+		console.log('daThis.action.attribute01=',daThis.action.attribute01, typeof daThis.action.attribute01);
+        var center_dialog = ( daThis.action.attribute01 == "true" ) ? true : false;
+		var margin = daThis.action.attribute02;
         // Logging
         var vLogging = true;
         if (vLogging) {
-            console.log('showTooltip: affectedElement:', vTabSelector);
-            console.log('showTooltip: Attribute Number of columns:', vNumcols);
-            console.log('showTooltip: Attribute Direction:', vDirection);
+            console.log('Resize Dialog: center_dialog=', center_dialog);
+            console.log('Resize Dialog: margin=', margin);
         }
-        apexreport2columns.distribute(vTabSelector,vNumcols,vDirection);
+        apexresizedialog.resize_dialog(center_dialog,margin);
     }
 };
